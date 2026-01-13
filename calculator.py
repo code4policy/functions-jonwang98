9a8767758fdf781a0377dc4dc111c839a4c40b23
jonwang98 <jonwang98@gmail.com>
2026-01-13 18:52:48 -0500

    added example of 5 / 6

--- calculator.py
+++ calculator.py
@@ -11,3 +11,7 @@ def divide(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return a / b
+
+print("I'm going use the calculator functions to divide 5 by 6")
+x = divide(5,6)
+print(x)
